3e3edbfdbc736ab77c292a6f5feef1549c69af8b
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-10-14 12:41:57 -0400

    chore: remove menu and sidebar components

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,5 @@
 import { ThemeProvider } from '@/providers/theme-provider';
 import { MenuProvider } from '@/providers/menu-provider';
-// import { Sidebar } from '@/components/sidebar/sidebar';
-// import { Menu } from '@/components/menu';
 import { Editor } from '@/components/editor';
 import { CommandPalette } from '@/components/command-palette';
 import { AuthProvider } from '@/providers/auth-provider';
@@ -11,8 +9,6 @@ export default function App() {
         <AuthProvider>
             <ThemeProvider>
                 <MenuProvider>
-                    {/* <Sidebar /> */}
-                    {/* <Menu /> */}
                     <Editor />
                     <CommandPalette />
                 </MenuProvider>
